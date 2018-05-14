b7caaabf610e7c54852d48af70531d0b85eb8a52
Rick Carback <rick@privategrity.com>
2018-05-14 18:08:33 +0000

    Add comments to clear up questions from code review

--- storage/interface.go
+++ storage/interface.go
@@ -11,6 +11,8 @@ package storage
 // (e-mail, group, nickname, etc).
 type ValueType int
 
+// Note: because DB backends vary, and this list could have
+//       items added and removed, we are not using an iota on purpose.
 const (
 	Email ValueType = 0 // An e-mail address
 	Nick  ValueType = 1 // The user's nickname
@@ -23,6 +25,9 @@ type Storage interface {
 	// GetKey - Get a key based on the key id (retval of AddKey)
 	GetKey(keyId string) ([]byte, bool)
 
+	// NOTE: See doc on user discovery for these 2 functions.
+	//       At this time we can't do a high-security version because
+	//       we lack the anonymous return receipts.
 	// AddUserKey - Add a user id to keyId (not used in high security)
 	AddUserKey(userId uint64, keyId string) error
 	// GetUserKey - Get a user's keyId (not used in high security)
