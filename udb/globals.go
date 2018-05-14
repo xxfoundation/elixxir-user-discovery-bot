363a47aed45e1729e0dc33765de0815529484ff6
Rick Carback <rick@privategrity.com>
2018-05-14 14:41:52 +0000

    Add globals file

---
+++ udb/globals.go
@@ -0,0 +1,12 @@
+////////////////////////////////////////////////////////////////////////////////
+// Copyright © 2018 Privategrity Corporation                                   /
+//                                                                             /
+// All rights reserved.                                                        /
+////////////////////////////////////////////////////////////////////////////////
+
+package udb
+
+// The User Discovery Bot's userid & registrationn code
+// (this is global in cMix systems)
+const UDB_USERID = 13
+const UDB_NICK = "UDB"
