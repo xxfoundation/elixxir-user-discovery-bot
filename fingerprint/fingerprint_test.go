e921985a76f29a22b0a9fbdea23bbdce3fcefd4d
Rick Carback <rick@privategrity.com>
2018-05-14 03:53:06 +0000

    Add test for fingerprint

---
+++ fingerprint/fingerprint_test.go
@@ -0,0 +1,23 @@
+////////////////////////////////////////////////////////////////////////////////
+// Copyright © 2018 Privategrity Corporation                                   /
+//                                                                             /
+// All rights reserved.                                                        /
+////////////////////////////////////////////////////////////////////////////////
+
+package fingerprint
+
+import (
+	"testing"
+)
+
+func TestFingerprint(t *testing.T) {
+	expected := "lsXBLOhwWxCWIgDL31s5Hxo/qc+8mKZP4kT9tD/6iTM="
+	testval := []byte{
+		'T', 'h', 'i', 's', ' ', 'i', 's', ' ', 't', 'h', 'e', ' ',
+		't', 'e', 's', 't', 'v', 'a', 'l',
+	}
+	retval := Fingerprint(testval)
+	if retval != expected {
+		t.Errorf("Fingerprint failed, Expected: %s, Got: %s", expected, retval)
+	}
+}
