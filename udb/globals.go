12b2824404ca3ec0bb7e95bf694ed6e12b00164a
Spencer Brown <spencer@privategrity.com>
2018-07-05 15:16:43 -0700

    Fix build: migrate to user.ID type

--- udb/globals.go
+++ udb/globals.go
@@ -7,12 +7,13 @@
 package udb
 
 import (
+	"gitlab.com/privategrity/client/user"
 	"gitlab.com/privategrity/user-discovery-bot/storage"
 )
 
 // The User Discovery Bot's userid & registrationn code
 // (this is global in cMix systems)
-const UDB_USERID = 13
+const UDB_USERID = user.ID(13)
 const UDB_NICK = "UDB"
 
 var DataStore storage.Storage
