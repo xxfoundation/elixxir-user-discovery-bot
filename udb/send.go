3d360befac8ef1cf7543904c515609c1a4a0838e
jaketaylor <jake@privategrity.com>
2018-08-03 12:49:20 -0400

    Fixed message response

--- udb/send.go
+++ udb/send.go
@@ -9,23 +9,23 @@ package udb
 
 import (
 	jww "github.com/spf13/jwalterweatherman"
-	client "gitlab.com/privategrity/client/api"
+	"gitlab.com/privategrity/client/io"
 	"gitlab.com/privategrity/client/parse"
-	"gitlab.com/privategrity/crypto/format" // <-- FIXME: this is annoying, WHY?
+	"gitlab.com/privategrity/client/user"
 )
 
 // Sender interface -- the api is broken here (does not return the error), so
 // we reimplement a new interface...
 type Sender interface {
-	Send(messageInterface format.MessageInterface) error
+	Send(userID user.ID, msg string) error
 }
 
 // ApiSender calls the api send function
 type APISender struct{}
 
 // Send calls the api send function
-func (a APISender) Send(message format.MessageInterface) error {
-	return client.Send(message)
+func (a APISender) Send(userID user.ID, msg string) error {
+	return io.Messaging.SendMessage(userID, msg)
 }
 
 // UdbSender is the sender interface to use
@@ -34,21 +34,13 @@ var UdbSender Sender = APISender{}
 // Wrap the API Send function (useful for mock tests)
 func Send(userID uint64, msg string, msgType parse.Type) {
 	// Create the message body and assign its type
-	msgBody := &parse.TypedBody{
+	message := string(parse.Pack(&parse.TypedBody{
 		Type: msgType,
 		Body: []byte(msg),
-	}
-	myID := uint64(UDB_USERID)
-	messages, err := format.NewMessage(myID, userID, msg)
-	if err != nil {
-		jww.FATAL.Panicf("Error creating message: %d, %d, %s",
-			myID, userID, msg)
-	}
-
-	for i := range messages {
-		sendErr := UdbSender.Send(messages[i])
-		if sendErr != nil {
-			jww.ERROR.Printf("Error responding to %d", userID)
-		}
+	}))
+	// Send the message
+	sendErr := UdbSender.Send(user.ID(userID), message)
+	if sendErr != nil {
+		jww.ERROR.Printf("Error responding to %d: %s", userID, sendErr)
 	}
 }
