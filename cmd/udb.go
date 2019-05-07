d0a41a64019cc33979a729a6841e56b695eaf6a3
Jono <jono@privategrity.com>
2019-05-07 09:41:28 -0700

    Create outputDsaPubKeyToJson() that encodes the user ID and DSA public key to JSON and outputs it to a file

--- cmd/udb.go
+++ cmd/udb.go
@@ -11,12 +11,18 @@
 package cmd
 
 import (
+	"crypto/rand"
+	"encoding/json"
+	"github.com/mitchellh/go-homedir"
+	jww "github.com/spf13/jwalterweatherman"
 	"gitlab.com/elixxir/client/api"
 	"gitlab.com/elixxir/crypto/certs"
 	"gitlab.com/elixxir/crypto/cyclic"
+	"gitlab.com/elixxir/crypto/signature"
 	"gitlab.com/elixxir/primitives/id"
 	"gitlab.com/elixxir/user-discovery-bot/storage"
 	"gitlab.com/elixxir/user-discovery-bot/udb"
+	"io/ioutil"
 	"os"
 )
 
@@ -101,10 +107,49 @@ func Init(sessionFile string, regCode string, grpConf string) *id.User {
 		udb.Log.DEBUG.Printf("UDB registered as user %v", *userId)
 	}
 
+	// Save DSA public key to JSON file
+	outputDsaPubKeyToJson(userId, ".elixxir", "udb_info.json")
+
 	return userId
 }
 
 // Log into the server using the user id generated from Init
 func Login(userId *id.User) {
 	clientObj.Login(userId, "", GATEWAY_ADDRESSES[0], certs.GatewayTLS)
+}
+
+// outputDsaPubKeyToJson encodes the DSA public key and user ID to JSON and
+// outputs it to the specified directory with the specified file name.
+func outputDsaPubKeyToJson(userID *id.User, dir, fileName string) {
+
+	// Get the default parameters and generate a public key from it
+	dsaParams := signature.GetDefaultDSAParams()
+	publicKey := dsaParams.PrivateKeyGen(rand.Reader).PublicKeyGen()
+
+	// Setup struct that will dictate the JSON structure
+	jsonStruct := struct {
+		Id             *id.User
+		Dsa_public_key *signature.DSAPublicKey
+	}{
+		Id:             userID,
+		Dsa_public_key: publicKey,
+	}
+
+	// Generate JSON from structure
+	data, err := json.MarshalIndent(jsonStruct, "", "\t")
+	if err != nil {
+		jww.ERROR.Printf("Error encoding structure to JSON: %s", err)
+	}
+
+	// Get the user's home directory
+	homeDir, err := homedir.Dir()
+	if err != nil {
+		jww.ERROR.Printf("Unable to retrieve user's home directory: %s", err)
+	}
+
+	// Write JSON to file
+	err = ioutil.WriteFile(homeDir+"/"+dir+"/"+fileName, data, 0644)
+	if err != nil {
+		jww.ERROR.Printf("Error writing JSON file: %s", err)
+	}
 }
