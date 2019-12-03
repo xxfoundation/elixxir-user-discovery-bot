9a7024dd558560a5129903cc10d6d0c0b04b1071
Konstantino Sparakis <ksparakis@elixxir.io>
2019-12-03 13:26:21 -0500

    undo last commit

--- cmd/udb.go
+++ cmd/udb.go
@@ -158,7 +158,6 @@ func getLatestMessageID() string {
 			//ERROR getting host log it here
 			//Needs to be part of a larger discussion for error handling
 			globals.Log.WARN.Printf("Failed to find the host with ID %v", receiveGateway.String())
-
 			continue
 		}
 
