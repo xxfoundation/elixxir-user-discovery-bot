d11532d3fbe8ccf0c113ad3215c199ddb7f21143
Konstantino Sparakis <ksparakis@elixxir.io>
2019-12-05 15:48:10 -0500

    remove binary builder

--- buildBin.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# My first script
-
-rm release/*
-GOOS=darwin GOARCH=amd64 CGO_ENABLED=0 go build -ldflags "-w -s" -o release/registration.darwin64 main.go
-mv release/registration.darwin64 release/udb.binary
-cp release/udb.binary ../localenvironment/binaries
