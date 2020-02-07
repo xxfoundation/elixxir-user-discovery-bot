b3fb3be4cff174bceb92a160eb418e26e64fe42f
jaketaylor <jake@privategrity.com>
2020-02-07 15:22:23 -0800

    changed how version commands work

--- cmd/version.go
+++ cmd/version.go
@@ -4,32 +4,43 @@
 // All rights reserved.                                                        /
 ////////////////////////////////////////////////////////////////////////////////
 
+// Handles command-line version functionality
+
 package cmd
 
 import (
 	"fmt"
 	"github.com/spf13/cobra"
+	"gitlab.com/elixxir/primitives/utils"
 )
 
-//go:generate go run gen.go
-// The above generates: GITVERSION, GLIDEDEPS, and SEMVER
+// Change this value to set the version for this build
+const currentVersion = "1.0.0"
+
+func printVersion() {
+	fmt.Printf("Elixxir User Discovery Bot v%s -- %s\n\n", SEMVER, GITVERSION)
+	fmt.Printf("Dependencies:\n\n%s\n", DEPENDENCIES)
+}
 
 func init() {
 	RootCmd.AddCommand(versionCmd)
-}
-
-func printVersion() {
-	fmt.Printf("Elixxir User Discovery Bot v%s -- %s\n\n", SEMVER,
-		GITVERSION)
-	fmt.Printf("Dependencies:\n\n%s\n", GLIDEDEPS)
+	RootCmd.AddCommand(generateCmd)
 }
 
 var versionCmd = &cobra.Command{
 	Use:   "version",
-	Short: "Print the version number of Elixxir UDB",
-	Long: `Print the version number of Elixxir User Discovery bot. This
-also prints the glide cache versions of all of its dependencies.`,
+	Short: "Print the version and dependency information for the Elixxir binary",
+	Long:  `Print the version and dependency information for the Elixxir binary`,
 	Run: func(cmd *cobra.Command, args []string) {
 		printVersion()
+	},
+}
+
+var generateCmd = &cobra.Command{
+	Use:   "generate",
+	Short: "Generates version and dependency information for the Elixxir binary",
+	Long:  `Generates version and dependency information for the Elixxir binary`,
+	Run: func(cmd *cobra.Command, args []string) {
+		utils.GenerateVersionFile(currentVersion)
 	},
 }
--- go.mod
+++ go.mod
@@ -13,9 +13,11 @@ require (
 	github.com/spf13/cobra v0.0.5
 	github.com/spf13/jwalterweatherman v1.1.0
 	github.com/spf13/viper v1.6.2
-	gitlab.com/elixxir/client v1.1.1-0.20200206230443-e77ca12cf7aa
+	gitlab.com/elixxir/client v1.1.1-0.20200206231036-aa1e40f18ade
 	gitlab.com/elixxir/comms v0.0.0-20200206201144-aa6e356b3770
 	gitlab.com/elixxir/crypto v0.0.0-20200206203107-b8926242da23
-	gitlab.com/elixxir/primitives v0.0.0-20200131183153-e93c6b75019f
+	gitlab.com/elixxir/primitives v0.0.0-20200207225613-9a4445ddec16
+	golang.org/x/crypto v0.0.0-20200207205829-a95e85b341fd // indirect
+	google.golang.org/genproto v0.0.0-20200207204624-4f3edf09f4f6 // indirect
 	mellium.im/sasl v0.2.1 // indirect
 )
