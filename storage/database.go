4ee9121ee0a591bc8690ffbd9a39bdd7a7aed953
jbhusson <jonah@elixxir.io>
2020-09-30 17:53:37 -0400

    Add initial db code

--- storage/database.go
+++ storage/database.go
@@ -1,135 +1,115 @@
-////////////////////////////////////////////////////////////////////////////////
-// Copyright © 2019 Privategrity Corporation                                   /
-//                                                                             /
-// All rights reserved.                                                        /
-////////////////////////////////////////////////////////////////////////////////
 package storage
 
 import (
-	"github.com/go-pg/pg"
-	"github.com/go-pg/pg/orm"
-	"gitlab.com/elixxir/client/globals"
-	"gitlab.com/xx_network/primitives/id"
+	"fmt"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
+	jww "github.com/spf13/jwalterweatherman"
+	"sync"
+	"time"
 )
+
+var UserDiscoveryDB Storage
+
+// Interface declaration for storage methods
+type Storage interface {
+}
 
 // Struct implementing the Database Interface with an underlying DB
 type DatabaseImpl struct {
-	db *pg.DB // Stored database connection
+	db *gorm.DB // Stored database connection
 }
 
-var UserDiscoveryDb Database
-
-type Database interface {
-	// Insert or Update a User into the database
-	UpsertUser(user *User) error
-	// Fetch a User from the database by ID
-	GetUser(id *id.ID) (*User, error)
-	// Fetch a User from the database by Value
-	GetUserByValue(value string) (*User, error)
-	// Fetch a User from the database by KeyId
-	GetUserByKeyId(keyId string) (*User, error)
-	//Delete a user
-	DeleteUser(id *id.ID) error
+// Struct implementing the Database Interface with an underlying Map
+type MapImpl struct {
+	sync.RWMutex
 }
 
-// Struct representing the udb_users table in the database
 type User struct {
-	// Overwrite table name
-	tableName struct{} `sql:"udb_users,alias:udb_users"`
-
-	// User Id
-	Id []byte `sql:",pk,unique"`
-	// Identifying informationgo-pg
-	Value string `sql:",unique"`
-	// Type of identifying information as denoted by the ValueType type
-	ValueType int
-	// Hash of the User public key
-	KeyId string `sql:",unique"`
-	// User public key
-	Key []byte `sql:",unique"`
+	Id        []byte `gorm:"primary_key"`
+	RsaPub    []byte
+	DhPub     []byte
+	Salt      []byte
+	Signature []byte
+	Facts     []Fact `gorm:"foreignKey:UserId"`
 }
 
-// Initialize a new User object
-func NewUser() *User {
-	return &User{
-		Id:        make([]byte, id.ArrIDLen),
-		Value:     "",
-		ValueType: -1,
-		KeyId:     "",
-		Key:       make([]byte, 0),
-	}
-}
-
-func (u *User) SetID(id *id.ID) {
-	u.Id = id.Marshal()
-}
-
-func (u *User) SetValue(val string) {
-	u.Value = val
-}
-
-func (u *User) SetValueType(valType int) {
-	u.ValueType = valType
-}
-
-func (u *User) SetKeyID(keyID string) {
-	u.KeyId = keyID
-}
-
-func (u *User) SetKey(key []byte) {
-	u.Key = key
+type Fact struct {
+	Id                 uint64 `gorm:"primary_key;AUTO_INCREMENT:true"`
+	UserId             []byte `gorm:"NOT NULL"`
+	Fact               string `gorm:"NOT NULL"`
+	FactType           uint64 `gorm:"NOT NULL"`
+	FactHash           []byte `gorm:"NOT NULL"`
+	Signature          []byte `gorm:"NOT NULL"`
+	ConfirmationId     []byte `gorm:"UNIQUE;NOT NULL"`
+	VerificationStatus uint64 `gorm:"NOT NULL"`
+	Manual             bool   `gorm:"NOT NULL"`
+	Code               uint64
+	Timestamp          time.Time `gorm:"NOT NULL"`
 }
 
 // Initialize the Database interface with database backend
-func NewDatabase(username, password, database, address string) Database {
-	// Create the database connection
-	db := pg.Connect(&pg.Options{
-		User:         username,
-		Password:     password,
-		Database:     database,
-		Addr:         address,
-		MaxRetries:   10,
-		MinIdleConns: 1,
-	})
+// Returns a Storage interface, Close function, and error
+func NewDatabase(username, password, database, address,
+	port string) (Storage, func() error, error) {
+	var err error
+	var db *gorm.DB
+	//connect to the database if the correct information is provided
+	if address != "" && port != "" {
+		// Create the database connection
+		connectString := fmt.Sprintf(
+			"host=%s port=%s user=%s dbname=%s sslmode=disable",
+			address, port, username, database)
+		// Handle empty database password
+		if len(password) > 0 {
+			connectString += fmt.Sprintf(" password=%s", password)
+		}
+		db, err = gorm.Open("postgres", connectString)
+	}
 
-	// Initialize the schema
-	err := createSchema(db)
-	if err != nil {
-		// If an error is thrown with the database, run with a map backend
-		globals.Log.ERROR.Printf("Unable to initalize database backend: %+v", err)
-		globals.Log.INFO.Println("Using map backend for User Discovery!")
-		return &MapImpl{
-			Users: make(map[*id.ID]*User),
+	// Return the map-backend interface
+	// in the event there is a database error or information is not provided
+	if (address == "" || port == "") || err != nil {
+
+		if err != nil {
+			jww.WARN.Printf("Unable to initialize database backend: %+v", err)
+		} else {
+			jww.WARN.Printf("Database backend connection information not provided")
+		}
+
+		defer jww.INFO.Println("Map backend initialized successfully!")
+
+		mapImpl := &MapImpl{}
+
+		return Storage(mapImpl), func() error { return nil }, nil
+	}
+
+	// Initialize the database logger
+	db.SetLogger(jww.TRACE)
+	db.LogMode(true)
+
+	// SetMaxIdleConns sets the maximum number of connections in the idle connection pool.
+	db.DB().SetMaxIdleConns(10)
+	// SetMaxOpenConns sets the maximum number of open connections to the database.
+	db.DB().SetMaxOpenConns(100)
+	// SetConnMaxLifetime sets the maximum amount of time a connection may be reused.
+	db.DB().SetConnMaxLifetime(24 * time.Hour)
+
+	// Initialize the database schema
+	// WARNING: Order is important. Do not change without database testing
+	models := []interface{}{User{}, Fact{}}
+	for _, model := range models {
+		err = db.AutoMigrate(model).Error
+		if err != nil {
+			return Storage(&DatabaseImpl{}), func() error { return nil }, err
 		}
 	}
 
-	// Return the database-backed Database interface
-	globals.Log.INFO.Println("Using database backend for User Discovery!")
-	return &DatabaseImpl{
+	// Build the interface
+	di := &DatabaseImpl{
 		db: db,
 	}
-}
 
-// Create the database schema
-func createSchema(db *pg.DB) error {
-	for _, model := range []interface{}{&User{}} {
-		err := db.CreateTable(model, &orm.CreateTableOptions{
-			// Ignore create table if already exists?
-			IfNotExists: true,
-			// Create temporary table?
-			Temp: false,
-			// FKConstraints causes CreateTable to create foreign key constraints
-			// for has one relations. ON DELETE hook can be added using tag
-			// `sql:"on_delete:RESTRICT"` on foreign key field.
-			FKConstraints: false,
-			// Replaces PostgreSQL data type `text` with `varchar(n)`
-			// Varchar: 255
-		})
-		if err != nil {
-			// Return error if one comes up
-			return err
-		}
-	}
-	// No error, return nil
-	return nil
+	jww.INFO.Println("Database backend initialized successfully!")
+	return Storage(di), db.Close, nil
 }
