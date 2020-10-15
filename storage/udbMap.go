fe48f40fcc3b1eaa83e48dc42cd14f05e7df2620
jbhusson <jonah@elixxir.io>
2020-10-15 07:27:48 -0700

    finish map impl

--- storage/udbMap.go
+++ storage/udbMap.go
@@ -10,7 +10,9 @@ package storage
 
 import (
 	"errors"
+	jww "github.com/spf13/jwalterweatherman"
 	"gitlab.com/xx_network/primitives/id"
+	"time"
 )
 
 func (m *MapImpl) CheckUser(username string, id *id.ID, rsaPem string) error {
@@ -63,6 +65,9 @@ func (m *MapImpl) InsertFact(fact *Fact) error {
 }
 
 func (m *MapImpl) VerifyFact(factHash []byte) error {
+	fid := &factId{}
+	copy(fid[:], factHash)
+	m.facts[*fid].Verified = true
 	return nil
 }
 
@@ -75,12 +80,51 @@ func (m *MapImpl) DeleteFact(confirmationId []byte) error {
 }
 
 func (m *MapImpl) InsertFactTwilio(userID, factHash, signature []byte, fact string, factType uint, confirmationID string) error {
+	f := Fact{
+		FactHash:  factHash,
+		UserId:    userID,
+		Fact:      fact,
+		FactType:  uint8(factType),
+		Signature: signature,
+		Verified:  false,
+		Timestamp: time.Now(),
+	}
+	tv := TwilioVerification{
+		ConfirmationId: confirmationID,
+		FactHash:       factHash,
+	}
+	fid := factId{}
+	copy(fid[:], factHash)
+	m.facts[fid] = &f
+	m.twilioVerifications[confirmationID] = &tv
 	return nil
 }
 func (m *MapImpl) VerifyFactTwilio(confirmationId string) error {
+	fid := factId{}
+	copy(fid[:], m.twilioVerifications[confirmationId].FactHash)
+	m.facts[fid].Verified = true
+	delete(m.twilioVerifications, confirmationId)
 	return nil
 }
 
 func (m *MapImpl) Search(factHashs [][]byte) []*User {
-	return nil
+	users := map[id.ID]User{}
+	for _, h := range factHashs {
+		fid := factId{}
+		copy(fid[:], h)
+		if f, ok := m.facts[fid]; ok {
+			uid, err := id.Unmarshal(f.UserId)
+			if err != nil {
+				jww.ERROR.Print("Failed to decode uid %+v: %+v", f.UserId, err)
+			}
+			users[*uid] = User{
+				Id: uid.Marshal(),
+			}
+		}
+	}
+	var result []*User
+	for _, u := range users {
+		result = append(result, &u)
+	}
+	return result
 }
