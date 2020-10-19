e2724abffd41648e1ff542abe9b22f4f3b2bb343
josh <josh@elixxir.io>
2020-10-19 11:38:32 -0700

    Update impl signature

--- udb/impl.go
+++ udb/impl.go
@@ -9,14 +9,15 @@ import (
 	pb "gitlab.com/elixxir/comms/mixmessages"
 	"gitlab.com/elixxir/comms/udb"
 	"gitlab.com/elixxir/user-discovery-bot/storage"
+	"gitlab.com/xx_network/comms/connect"
 	"gitlab.com/xx_network/comms/messages"
 )
 
-func NewImplementation() *udb.Implementation  {
+func NewImplementation() *udb.Implementation {
 	impl := udb.NewImplementation()
 
-	impl.Functions.RegisterUser = func(registration *pb.UDBUserRegistration) (*messages.Ack, error) {
-		return RegisterUser(registration, clientObj, storage.UserDiscoveryDB)
+	impl.Functions.RegisterUser = func(registration *pb.UDBUserRegistration, auth *connect.Auth) (*messages.Ack, error) {
+		return RegisterUser(registration, clientObj, storage.UserDiscoveryDB, auth)
 	}
 
 	return impl
