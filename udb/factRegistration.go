bc29af1ff9fd0b3a8bc3eca92ca2184635978ae0
Jono Wenger <jono@elixxir.io>
2020-10-20 11:38:25 -0700

    Add test for RegisterFact

--- udb/factRegistration.go
+++ udb/factRegistration.go
@@ -1,7 +1,6 @@
 package udb
 
 import (
-	"fmt"
 	"github.com/pkg/errors"
 	pb "gitlab.com/elixxir/comms/mixmessages"
 	"gitlab.com/elixxir/crypto/hash"
@@ -13,17 +12,17 @@ import (
 )
 
 var (
-	invalidFactRegisterRequestError = errors.New("Unable to parse required fields in fact registration request.")
-	factExistsError                 = errors.New("Cannot register fact that already exists.")
-	noUserError                     = errors.New("User associated with fact not registered.")
-	invalidUserKeyError             = errors.New("Could not parse user's key.")
-	invalidFactSigError             = errors.New("Failed to verify fact signature.")
-	invalidUserIdError              = errors.New("Failed to parse user ID.")
-	twilioRegFailureError           = errors.New("Failed to register fact with Twilio.")
+	invalidFactRegisterRequestError = "Unable to parse required fields in fact registration request."
+	factExistsError                 = "Cannot register fact that already exists."
+	noUserError                     = "User associated with fact not registered."
+	invalidUserKeyError             = "Could not parse user's key."
+	invalidFactSigError             = "Failed to verify fact signature."
+	invalidUserIdError              = "Failed to parse user ID."
+	twilioRegFailureError           = "Failed to register fact with Twilio."
 )
 
 // RegisterFact is an endpoint that attempts to register a user's fact.
-func RegisterFact(request *pb.FactRegisterRequest, db storage.Storage,
+func RegisterFact(request *pb.FactRegisterRequest, verifier twilio.VerificationService, store storage.Storage,
 	auth *connect.Auth) (*pb.FactRegisterResponse, error) {
 
 	// Ensure client is properly authenticated
@@ -33,46 +32,46 @@ func RegisterFact(request *pb.FactRegisterRequest, db storage.Storage,
 
 	// Return an error if the request is invalid
 	if request == nil || request.Fact == nil {
-		return &pb.FactRegisterResponse{}, invalidFactRegisterRequestError
+		return &pb.FactRegisterResponse{}, errors.New(invalidFactRegisterRequestError)
 	}
 
 	// Return an error if the fact is already registered
 	hashedFact := request.Fact.Digest()
-	if len(storage.UserDiscoveryDB.Search([][]byte{hashedFact})) != 0 {
-		return &pb.FactRegisterResponse{}, factExistsError
+	if len(store.Search([][]byte{hashedFact})) != 0 {
+		return &pb.FactRegisterResponse{}, errors.New(factExistsError)
 	}
-	test := uint(5)
-	fmt.Println(test)
 
 	// Return an error if the fact's user is not registered
-	user, err := db.GetUser(request.UID)
+	user, err := store.GetUser(request.UID)
 	if err != nil {
-		return &pb.FactRegisterResponse{}, noUserError
+		return &pb.FactRegisterResponse{}, errors.Errorf("Failed to parse user ID: %+v", err)
+	} else if user == nil {
+		return &pb.FactRegisterResponse{}, errors.New(noUserError)
 	}
 
 	// Parse the client's public key
 	clientPubKey, err := rsa.LoadPublicKeyFromPem([]byte(user.RsaPub))
 	if err != nil {
-		return &pb.FactRegisterResponse{}, invalidUserKeyError
+		return &pb.FactRegisterResponse{}, errors.New(invalidUserKeyError)
 	}
 
 	// Return an error if the fact signature cannot be verified
 	err = rsa.Verify(clientPubKey, hash.CMixHash, hashedFact, request.FactSig, nil)
 	if err != nil {
-		return &pb.FactRegisterResponse{}, invalidFactSigError
+		return &pb.FactRegisterResponse{}, errors.New(invalidFactSigError)
 	}
 
 	// Marshal user ID
 	userID, err := id.Unmarshal(request.UID)
 	if err != nil {
-		return &pb.FactRegisterResponse{}, invalidUserIdError
+		return &pb.FactRegisterResponse{}, errors.New(invalidUserIdError)
 	}
 
 	// Register fact with Twilio to get confirmation ID
 	confirmationID, err := twilio.RegisterFact(userID, request.Fact.Fact,
-		uint8(request.Fact.FactType), request.FactSig, nil, db)
+		uint8(request.Fact.FactType), request.FactSig, verifier, store)
 	if err != nil {
-		return &pb.FactRegisterResponse{}, twilioRegFailureError
+		return &pb.FactRegisterResponse{}, errors.New(twilioRegFailureError)
 	}
 
 	// Create response
--- udb/factRegistration_test.go
+++ udb/factRegistration_test.go
@@ -1,33 +1,49 @@
 package udb
 
 import (
-	pb "gitlab.com/elixxir/comms/mixmessages"
 	"gitlab.com/elixxir/user-discovery-bot/storage"
-	"gitlab.com/xx_network/primitives/id"
+	"gitlab.com/elixxir/user-discovery-bot/twilio"
+	"gitlab.com/xx_network/comms/connect"
+	"gitlab.com/xx_network/crypto/signature/rsa"
 	"testing"
 )
 
+// Happy path.
 func TestRegisterFact(t *testing.T) {
-	db, _, err := storage.NewDatabase("", "", "", "", "11")
+	// Initialize client and storage
+	client := initTestClient(t)
+	store, _, _ := storage.NewDatabase("", "", "", "", "")
+
+	// Create a mock host
+	params := connect.GetDefaultHostParams()
+	params.MaxRetries = 0
+	fakeHost, err := connect.NewHost(client.GetCurrentUser(), "", nil, params)
 	if err != nil {
-		t.Fatalf("Failed to initialize mock database: %+v", err)
+		t.Errorf("Failed to create fakeHost, %s", err)
 	}
 
-	uid := id.NewIdFromString("zezima", id.User, t)
-
-	request := &pb.FactRegisterRequest{
-		UID: uid.Bytes(),
-		Fact: &pb.Fact{
-			Fact:     "Hair is blue",
-			FactType: 5,
-		},
-		FactSig: []byte("test"),
+	// Construct mock auth object
+	auth := &connect.Auth{
+		IsAuthenticated: true,
+		Sender:          fakeHost,
 	}
 
-	response, err := RegisterFact(request, db, nil)
+	err = store.InsertUser(&storage.User{
+		Id:     client.GetCurrentUser().Bytes(),
+		RsaPub: string(rsa.CreatePublicKeyPem(client.GetCommManager().Comms.GetPrivateKey().GetPublic())),
+	})
+	if err != nil {
+		t.Fatalf("Failed to insert user: %+v", err)
+	}
+
+	request := buildFactMessage("newUser123", client)
+
+	response, err := RegisterFact(request, twilio.MV, store, auth)
 	if err != nil {
 		t.Errorf("RegisterFact() produced an error: %+v", err)
 	}
 
-	t.Logf("response: %+v", response)
+	if response.ConfirmationID != "0" {
+		t.Errorf("RegisterFact() produced incorrect ConfirmationID: %s", response.ConfirmationID)
+	}
 }
