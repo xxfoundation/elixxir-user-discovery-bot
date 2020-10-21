c3b1b1debaf2caca6e7ff22bdd5742925ebe8c38
jbhusson <jonah@elixxir.io>
2020-10-21 11:23:57 -0700

    Make codes structure in mockverifier public so users can get the code to pass in

--- twilio/mockVerify.go
+++ twilio/mockVerify.go
@@ -14,22 +14,22 @@ import (
 )
 
 // Mocked verifier implementation
-var MV = &MockVerifier{codes: map[string]int{}, index: 0}
+var MV = &MockVerifier{Codes: map[string]int{}, index: 0}
 
 type MockVerifier struct {
-	codes map[string]int
+	Codes map[string]int
 	index int
 }
 
 func (v *MockVerifier) Verification(to, channel string) (string, error) {
 	cid := strconv.Itoa(v.index)
 	v.index++
-	v.codes[cid] = rand.Int()
+	v.Codes[cid] = rand.Int()
 	return cid, nil
 }
 
 func (v *MockVerifier) VerificationCheck(code int, to string) (bool, error) {
-	c, ok := v.codes[to]
+	c, ok := v.Codes[to]
 	if !ok || c != code {
 		return false, nil
 	}
