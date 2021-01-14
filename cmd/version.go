42c3d6e3088ef5e7c69fd9dd8efe99144bbb5a50
Richard T. Carback III <rick.carback@gmail.com>
2021-01-14 01:23:32 +0000

    Add version subcommand

---
+++ cmd/version.go
@@ -0,0 +1,50 @@
+///////////////////////////////////////////////////////////////////////////////
+// Copyright © 2020 xx network SEZC                                          //
+//                                                                           //
+// Use of this source code is governed by a license that can be found in the //
+// LICENSE file                                                              //
+///////////////////////////////////////////////////////////////////////////////
+
+// Handles command-line version functionality
+
+package cmd
+
+import (
+	"fmt"
+	"github.com/spf13/cobra"
+	"gitlab.com/elixxir/client/api"
+	"gitlab.com/xx_network/primitives/utils"
+)
+
+// Change this value to set the version for this build
+const currentVersion = "1.4.0"
+
+func Version() string {
+	out := fmt.Sprintf("Elixxir UDB v%s -- %s\n\n", api.SEMVER,
+		api.GITVERSION)
+	out += fmt.Sprintf("Dependencies:\n\n%s\n", api.DEPENDENCIES)
+	return out
+}
+
+func init() {
+	rootCmd.AddCommand(versionCmd)
+	rootCmd.AddCommand(generateCmd)
+}
+
+var versionCmd = &cobra.Command{
+	Use:   "version",
+	Short: "Print the version and dependency information for the Elixxir binary",
+	Long:  `Print the version and dependency information for the Elixxir binary`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Printf(Version())
+	},
+}
+
+var generateCmd = &cobra.Command{
+	Use:   "generate",
+	Short: "Generates version and dependency information for the Elixxir binary",
+	Long:  `Generates version and dependency information for the Elixxir binary`,
+	Run: func(cmd *cobra.Command, args []string) {
+		utils.GenerateVersionFile(currentVersion)
+	},
+}
