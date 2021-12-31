8cceec647bb46cff096264f700b807f72b5f5d27
Richard T. Carback III <rick.carback@gmail.com>
2021-12-31 01:35:01 +0000

    Revert "Revert "Merge branch 'Josh/ProtoClient' into 'dev'""
    This reverts commit 57901ad5fad09b6e4dad0a3028f808ff3ea31ab9.

--- interfaces/params/general.go
+++ interfaces/params/general.go
@@ -9,9 +9,11 @@
 package params
 
 type General struct {
-	SessionPath string
-	Ndf         string
-	PermCert    []byte
+	SessionPath       string
+	ProtoUserJsonPath string
+	ProtoUserJson     []byte
+	Ndf               string
+	PermCert          []byte
 
 	Database
 	IO
