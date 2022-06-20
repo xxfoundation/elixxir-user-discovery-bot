c06f5b1b3ab4be55cab5a9cfbbc8b19a143deff3
Jake Taylor <jake@elixxir.io>
2022-06-20 14:04:11 -0500

    update vers for master merge

--- cmd/version.go
+++ cmd/version.go
@@ -17,7 +17,7 @@ import (
 )
 
 // Change this value to set the version for this build
-const currentVersion = "2.2.0"
+const currentVersion = "2.3.0"
 
 func printVersion() {
 	fmt.Printf("xx network User Discovery v%s -- %s\n\n", SEMVER, GITVERSION)
