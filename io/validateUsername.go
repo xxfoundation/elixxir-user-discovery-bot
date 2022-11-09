8d6986eb670426723836bade58d67558bb066d24
joshemb <josh@elixxir.io>
2022-11-09 11:58:48 -0800

    Fix build issues from pulling release

--- io/validateUsername.go
+++ io/validateUsername.go
@@ -21,36 +21,47 @@ const (
 	usernameNotAssociatedWithUser = "username %s is not associated with user ID %s"
 )
 
-// ValidateUsername validates that a user owns a username by signing the contents of the
-// mixmessages.UsernameValidationRequest.
+// ValidateUsername validates that a user owns a username by signing the
+// contents of the mixmessages.UsernameValidationRequest.
 func validateUsername(request *pb.UsernameValidationRequest,
-	store *storage.Storage, privKey *rsa.PrivateKey, rng io.Reader) (*pb.UsernameValidation, error) {
+	store *storage.Storage, privKey *rsa.PrivateKey,
+	rng io.Reader) (*pb.UsernameValidation, error) {
 	// Return an error if the request is invalid
 	if request == nil || request.UserId == nil {
-		return &pb.UsernameValidation{}, errors.New("Unable to parse required " +
-			"fields in registration message")
+		return &pb.UsernameValidation{}, errors.New("Unable to parse " +
+			"required fields in registration message")
 	}
 
 	// Marshal user ID
 	userID, err := id.Unmarshal(request.UserId)
 	if err != nil {
-		return &pb.UsernameValidation{}, errors.WithMessage(err, invalidUserIdError)
+		return &pb.UsernameValidation{},
+			errors.WithMessage(err, invalidUserIdError)
 	}
 
 	// Return an error if the user is not registered
 	user, err := store.GetUser(request.UserId)
 	if err != nil {
-		return &pb.UsernameValidation{}, errors.WithMessage(err, getUserFailureError)
+		return &pb.UsernameValidation{},
+			errors.WithMessage(err, getUserFailureError)
 	} else if user == nil {
 		return &pb.UsernameValidation{}, errors.Errorf(noUserError,
 			userID)
 	}
 
+	// Parse the client's public key
+	clientPubKey, err := rsa.LoadPublicKeyFromPem([]byte(user.RsaPub))
+	if err != nil {
+		return &pb.UsernameValidation{},
+			errors.New("Could not parse key from storage")
+	}
+
 	// Create a signature verifying the user owns their username
 	verificationSignature, err := crust.SignVerification(rng, privKey,
-		user.Username, []byte(user.RsaPub))
+		user.Username, clientPubKey)
 	if err != nil {
-		return nil, errors.Errorf("Failed to create verification signature: %v", err)
+		return nil, errors.Errorf("Failed to create verification signature: %v",
+			err)
 	}
 
 	// Return signature to user
